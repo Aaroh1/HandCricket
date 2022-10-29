9217b96184ea3f641d21d8e80ec63538c49e4578
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-10-29 11:22:51 +0530

    batsman,bowler and team class completed

--- team.h
+++ team.h
@@ -21,22 +21,22 @@ class Team{
         cout<<"Player 4(Bowler)"<<endl;
         cout<<"Player 5(Bowler)"<<endl;
     }
-    void updateBatStats(int m, int runs, int i){
-        batsmen[i-1].setRuns(m,runs);
+    void updateBatStats(int m, int runs, int p){
+        batsmen[p-1].setRuns(m,runs);
     }
-    void updateBowlStats(int m, int runs, int w, int i, float overs){
-        bowlers[i-1].setRunsConceeded(m,runs);
-        bowlers[i-1].setWickets(m,w);
-        bowlers[i-1].setEconomy(m,overs);
+    void updateBowlStats(int m, int runs, int w, int p, float overs){
+        bowlers[p-1].setRunsConceeded(m,runs);
+        bowlers[p-1].setWickets(m,w);
+        bowlers[p-1].setEconomy(m,overs);
     }
     void displayMatchStats(int m, int i){
         cout<<"Team "<<i<<endl;
         for(int i=0;i<3;i++){
-            cout<<"Player "<<i+1<<setw(15)<<batsmen[i].getRuns(m);
+            cout<<"Batsman "<<i+1<<setw(15)<<batsmen[i].getRuns(m);
         }
         cout<<"\n\n";
         for(int i=0;i<2;i++){
-            cout<<"Player "<<i+4<<setw(15)<<bowlers[i].getRunsConceeded(m)<<"-"<<bowlers[i].getWickets(m)<<endl;
+            cout<<"Bowler "<<i+1<<setw(15)<<bowlers[i].getRunsConceeded(m)<<"-"<<bowlers[i].getWickets(m)<<endl;
         }
         cout<<endl;
     }
